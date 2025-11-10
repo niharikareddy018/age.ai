b8f748eb52aaf6b12f6a981712e63cecbc19b420
RA1911003020517 <mr4131@srmist.edu.in>
2025-11-10 21:07:10 +0530

    Fix import path for extensions

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from datetime import timedelta
 import os
 from dotenv import load_dotenv
-from extensions import db, jwt
+from backend.extensions import db, jwt
 
 load_dotenv()
 
